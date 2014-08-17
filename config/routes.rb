5cc334bd8c5b36899e0c6ca72932182e428a6fc5
Jean-Philippe Kha <jp.kha@live.fr>
2014-08-17 19:20:27 +0200

    init

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ FirstApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+ 
+  root :to => 'views#products#index'
+
 end
